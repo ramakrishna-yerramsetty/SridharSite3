7e2536ae1e40b765ad3b4ce5e59bafbf74d2ce27
ramakrishna-yerramsetty <kris.nitk5@gmail.com>
2022-01-05 21:31:51 +0530

    blinking visibility

---
+++ custom-js.js
@@ -0,0 +1,16 @@
+
+/*
+setTimeout(function() {
+    console.log("delayed");
+    while(true){
+        let i = Math.floor(Math.random() * 6) +1;
+        let selectedBulb = "bulb-"+i;
+        document.getElementById('bulb-1').toggle = false;
+        setTimeout({}, 1000);
+        document.getElementById('bulb-1').style.visbility = true;
+        console.log(i);
+        console.log('new');
+    }
+}, 5000);
+
+*/
